78d20e34ea50db5ca910ba6cde61e404204e28ca
Irum F <irummf@gmail.com>
2018-08-23 21:10:37 +0000

    automatically backed up by learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,8 +1,9 @@
 def rescue_princess_peach
-  status = "rescued"
-  puts "Hooray! Mario has rescued Princess Peach."
   rescue_princess_peach
   puts status
+  status = "rescued"
+  puts "Hooray! Mario has rescued Princess Peach."
+  
 end
 
 
